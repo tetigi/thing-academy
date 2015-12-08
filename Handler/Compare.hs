de7fbe1f16a65b6ec6678abafb6a41c753593ffe
Luke Tomlin <ltomlin@palantir.com>
2015-12-08 23:18:30 +0000

    Added a way of feeding the selections back into the request

--- Handler/Compare.hs
+++ Handler/Compare.hs
@@ -2,7 +2,16 @@ module Handler.Compare where
 
 import Import
 
+data This = This
+    deriving Show
+
 getCompareR :: Handler Html
 getCompareR = do
+    thisValue <- lookupGetParam "this"
+    thatValue <- lookupGetParam "that"
     defaultLayout $ do
+        setTitle $ case (thisValue, thatValue) of
+            (Just this, Just that)  -> "OH GOD"
+            _                       -> "Welcome to X-compare"
+        thisButton <- newIdent
         $(widgetFile "compare")
