d3398c981659ed5353b4b276edeffe9f6f3286fc
Luke Tomlin <ltomlin@palantir.com>
2015-12-10 09:51:08 +0000

    Figured out how basic persistent DB stuff works.

--- Handler/Compare.hs
+++ Handler/Compare.hs
@@ -1,5 +1,4 @@
 module Handler.Compare where
-
 import Import
 
 data This = This
@@ -7,11 +6,18 @@ data This = This
 
 getCompareR :: Handler Html
 getCompareR = do
+    entityID <- runDB $ insert $ ComparisonEntity "abcdef" "ZOMG" 100
+    entity <- runDB $ get404 entityID
+
     thisValue <- lookupGetParam "this"
     thatValue <- lookupGetParam "that"
+    whichValue <- lookupGetParam "which"
+    let thisThing = "THIS" :: Text
+        thatThing = "THAT" :: Text
+
     defaultLayout $ do
-        setTitle $ case (thisValue, thatValue) of
-            (Just this, Just that)  -> "OH GOD"
-            _                       -> "Welcome to X-compare"
+        setTitle $ case (thisValue, thatValue, whichValue) of
+            (Just this, Just that, Just which)  -> "OH GOD"
+            _                                   -> "Welcome to X-compare"
         thisButton <- newIdent
         $(widgetFile "compare")
