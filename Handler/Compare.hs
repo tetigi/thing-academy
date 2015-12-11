bb25e4a142bc4d89f586a2aef61b5eb074a97dcf
Luke Tomlin <ltomlin@palantir.com>
2015-12-11 14:06:32 +0000

    ELO ranking now works and properly updates the DB

--- Handler/Compare.hs
+++ Handler/Compare.hs
@@ -5,26 +5,49 @@ import Data.Random
 import Data.Random.Source.DevRandom
 import Data.Random.Extras
 
+import Logic.Elo
+
 data This = This
     deriving Show
 
 getCompareR :: Handler Html
 getCompareR = do
+    -- Get all entities from the db
     entities <- runDB $ selectList [] [Asc ComparisonElo]
 
+    -- Pick two entities at random
     Entity _ thisThingEntity <- liftIO $ runRVar (choice entities) DevRandom
     Entity _ thatThingEntity <- liftIO $ runRVar (choice entities) DevRandom
 
+    -- Expose the variables for insertion into the HTML
     let thisThing = comparisonValue thisThingEntity
+        thisHash  = comparisonHash thisThingEntity
         thatThing = comparisonValue thatThingEntity
+        thatHash  = comparisonHash thatThingEntity
 
+    liftIO $ putStrLn thisHash
+
+    -- Check for provided query parameters for a previous comparison
     thisValue <- lookupGetParam "this"
     thatValue <- lookupGetParam "that"
     whichValue <- lookupGetParam "which"
 
+    -- Check if the query parameters actually contain anything
+    _ <- case (thisValue, thatValue, whichValue) of
+        (Just this, Just that, Just which)  -> do
+            Entity thisId thisThingEntity' <- runDB $ getBy404 $ UniqueHash this
+            Entity thatId thatThingEntity' <- runDB $ getBy404 $ UniqueHash that
+            case which of
+                "this"  ->
+                    let (thisElo, thatElo) = getNewEloPair (comparisonElo thisThingEntity') (comparisonElo thatThingEntity') PlayerOneWin in
+                    runDB $ sequence [update thisId [ComparisonElo =. thisElo], update thatId [ComparisonElo =. thatElo]]
+                "that"  ->
+                    let (thisElo, thatElo) = getNewEloPair (comparisonElo thisThingEntity') (comparisonElo thatThingEntity') PlayerTwoWin in
+                    runDB $ sequence [update thisId [ComparisonElo =. thisElo], update thatId [ComparisonElo =. thatElo]]
+                _       -> return [()]
+        _               -> return [()]
+
+    -- Start laying out the webpage
     defaultLayout $ do
-        setTitle $ case (thisValue, thatValue, whichValue) of
-            (Just this, Just that, Just which)  -> "OH GOD"
-            _                                   -> "Welcome to X-compare"
-        thisButton <- newIdent
+        -- Fill in the rest with compare.hamlet
         $(widgetFile "compare")
