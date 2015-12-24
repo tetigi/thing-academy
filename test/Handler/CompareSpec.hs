9e57f6411fd35cd4b89082146fcdd61f7441a02e
tetigi <luke.tomlin@gmail.com>
2015-12-24 16:14:57 +0000

    Wrote the tests. It's slow though..

--- test/Handler/CompareSpec.hs
+++ test/Handler/CompareSpec.hs
@@ -12,8 +12,40 @@ spec = withApp $ do
         _ <- seedDB
         get CompareR
         statusIs 200
-    {--
-    it "throws an error with incorrect hash"
-    it "still works when invalid which is provided"
-    it "updates the ELO correctly"
-    --}
+    it "loads the new hash successfully if present" $ do
+        _ <- seedDB
+        request $ do
+            setMethod "GET"
+            setUrl CompareR
+            addGetParam "this" "abc123"
+            addGetParam "that" "xyz987"
+            addGetParam "which" "that"
+        statusIs 200
+    it "still works when invalid which is provided" $ do
+        _ <- seedDB
+        request $ do
+            setMethod "GET"
+            setUrl CompareR
+            addGetParam "this" "abc123"
+            addGetParam "that" "xyz987"
+            addGetParam "which" "foobarbaz"
+        statusIs 200
+    it "updates the ELO correctly" $ do
+        _ <- seedDB
+        request $ do
+            setMethod "GET"
+            setUrl CompareR
+            addGetParam "this" "abc123"
+            addGetParam "that" "xyz987"
+            addGetParam "which" "this"
+        Just (Entity _ thisThingEntity) <- runDB $ getBy $ UniqueHash "abc123"
+        assertEqual "incorrect hash value" 1516 (comparisonElo thisThingEntity)
+    it "throws a 500 error with incorrect hash" $ do
+        _ <- seedDB
+        request $ do
+            setMethod "GET"
+            setUrl CompareR
+            addGetParam "this" "a"
+            addGetParam "that" "b"
+            addGetParam "which" "that"
+        statusIs 500
