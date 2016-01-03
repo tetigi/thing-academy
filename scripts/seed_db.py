267d117d46b51aee81f3b3dbc367c1dd0451c738
tetigi <luke.tomlin@gmail.com>
2016-01-03 22:41:27 +0000

    Updated seed script

--- scripts/seed_db.py
+++ scripts/seed_db.py
@@ -1,18 +1,22 @@
 import glob
 import hashlib
-import sqlite3
+import psycopg2
 import sys
 
-# called as `python seed_db.py <db_file> <location_of_images>
+# called as `python seed_db.py <connection string> <location_of_images>
 
 def simple_hash(s):
     return hashlib.md5(s.encode()).hexdigest()[0:10]
 
-conn = sqlite3.connect(sys.argv[1])
+conn = psycopg2.connect(sys.argv[1])
+cur = conn.cursor()
+
 values = []
 for i,img in enumerate(glob.glob(sys.argv[2] + '/*')):
     values.append((i+1, simple_hash(img), img, 1500))
 
-conn.executemany('INSERT INTO comparison VALUES (?,?,?,?)', values)
+cur.executemany('INSERT INTO comparison VALUES (?,?,?,?)', values)
 conn.commit()
+
+cur.close()
 conn.close()
